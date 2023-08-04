94df9ca9bfa52917363d9e10d638558fd8703a81
andrei.curta <andrei.curta@ensemble.com>
2023-08-04 20:56:36 +0300

    gatsby setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env`,
+})
+
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
@@ -5,5 +9,30 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    // {
+    //   resolve: 'gatsby-source-google-spreadsheets',
+    //   options: {
+    //     credentials: {
+    //       type: 'service_account',
+    //       project_id: process.env.PROJECT_ID,
+    //       private_key_id: process.env.PRIVATE_KEY_ID,
+    //       private_key: process.env.PRIVATE_KEY.replace(/(\\r)|(\\n)/g, '\n'),
+    //       client_email: process.env.CLIENT_EMAIL,
+    //       client_id: '',
+    //       auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+    //       token_uri: 'https://oauth2.googleapis.com/token',
+    //       auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+    //       client_x509_cert_url: `https://www.googleapis.com/robot/v1/metadata/x509/${process.env.PROJECT_ID}%40appspot.gserviceaccount.com`,
+    //     },
+    //   }
+    // },
+    {
+      resolve: 'gatsby-source-google-spreadsheets',
+      options: {
+        spreadsheetId: '1EgZbxlPqe1ErEjAeAFKuQpKm1c0CjeQ9DrWkZnXUqOM',
+        credentials: require('./google-credentials.json')
+      }
+    },
+  ],
 }
